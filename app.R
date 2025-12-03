802a7dc1a368908566472a8c6471c381d8e789b8
Sam Swanson (TO) <Sam.Swanson@shakopeedakota.org>
2025-12-03 07:33:58 -0600

    Switch to golem loading

--- app.R
+++ app.R
@@ -2,6 +2,6 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
+library(macroibi)
 macroibi::run_macroibi(demo_mode = TRUE) # add parameters here (if any)
