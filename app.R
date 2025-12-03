9c3364a50ca78917911b4f0d4141ec6a1cecfeca
Sam Swanson (TO) <Sam.Swanson@shakopeedakota.org>
2025-12-03 09:24:17 -0600

    Add deployment infrastructure and namespace internal functions

--- app.R
+++ app.R
@@ -1,6 +1,2 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
 
-options( "golem.app.prod" = TRUE)
-macroibi::run_macroibi(demo_mode = TRUE) # add parameters here (if any)
+macroibi::run_macroibi(demo_mode = TRUE)
---
+++ tools/deploy_app.R
@@ -0,0 +1,13 @@
+options(renv.config.auto.activate = FALSE)
+Sys.unsetenv("RENV_PROJECT")
+
+options(repos = c(CRAN = "https://cran.r-project.org"))
+
+getOption("repos")
+
+rsconnect::deployApp(
+  appDir        = ".",
+  appName       = "MacroIBI",
+  account       = "smsc2",
+  server        = "shinyapps.io",
+)
